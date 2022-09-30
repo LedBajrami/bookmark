610ae43de57a4b9f79b9fe0d2753598a0dc55734
LedBajrami <108076169+LedBajrami@users.noreply.github.com>
2022-09-30 21:11:08 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
+
+
+//collapbsibleTable
+let coll = document.getElementsByClassName("collapsible-button");
+let i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
